36de87400a863ed64fe6e5ab150107e283767734
John Dyer <johntdyer@gmail.com>
2015-02-03 14:08:56 -0500

    update handle to be a little more simplistic

--- http-catcher.go
+++ http-catcher.go
@@ -5,30 +5,13 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"regexp"
 )
 
-func HomeHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-
+func BackendHandler(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
 	w.Header().Set("X-Tropo-Backend-Path", req.URL.Path)
 	w.Header().Set("X-Tropo-Backend-Protocol", req.Proto)
 	w.Header().Set("X-Tropo-Backend", req.Header.Get("X-Tropo-Backend"))
-
-	var rest = regexp.MustCompile(`^/rest.*`)
-	var session = regexp.MustCompile(`^/session.*`)
-
-	switch {
-	case rest.MatchString(req.URL.Path):
-		log.Debug("Path: " + req.URL.Path + " matches provisioning")
-		fmt.Fprintf(w, "you got rest\n")
-	case session.MatchString(req.URL.Path):
-		log.Debug("Path: " + req.URL.Path + " matches sessions")
-		fmt.Fprintf(w, "you got sessions\n")
-	default:
-		log.Debug("Path: " + req.URL.Path + " has no matches")
-		http.Error(w, "fail boat\n", http.StatusServiceUnavailable)
-	}
-
+	fmt.Fprintf(w, "you got %s\n", ps.ByName("name"))
 }
 
 func init() {
@@ -43,7 +26,10 @@ func init() {
 }
 
 func main() {
-	router := httprouter.New()
-	router.GET("/*wildcard", HomeHandler)
-	log.Fatal(http.ListenAndServe(":8081", router))
+
+	client := httprouter.New()
+	client.GET("/*name", BackendHandler)
+
+	http.ListenAndServe("localhost:9082", client)
+
 }
